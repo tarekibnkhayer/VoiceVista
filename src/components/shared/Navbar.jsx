d721f3c5514b3322a7804b1829e7e6caf2556087
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-11-24 20:13:49 +0600

    update navbar content

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,14 +1,28 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
+	const navLinks = <>
+	<NavLink to="/" className="ml-5">Home</NavLink>
+	<NavLink to="/surveys" className="ml-5">Surveys</NavLink>
+	<NavLink to="/surveyDetails" className="ml-5">SurveyDetails</NavLink>
+	<NavLink to="/pro" className="ml-5">Pro</NavLink>
+	</>
   return (
     <div>
-      <div className="navbar navbar-glass navbar-sticky">
+      <div className="navbar navbar-glass navbar-sticky hidden lg:flex">
 	<div className="navbar-start">
-		<a className="navbar-item">Ripple UI</a>
+	<img
+            src="https://i.imgur.com/Ci5vu8p.png"
+            alt="website logo"
+            className="w-14"
+          />
+	</div>
+	<div className="navbar-center">
+		{navLinks}
 	</div>
 	<div className="navbar-end">
-		<a className="navbar-item">Home</a>
+		<NavLink to="/login">Login</NavLink>
 	</div>
 </div>
     </div>
