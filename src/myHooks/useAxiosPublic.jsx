60c68e345800ec3d43ebc70c7a1a67f697be3bd1
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-11-29 21:37:38 +0600

    deploying to firebase

--- src/myHooks/useAxiosPublic.jsx
+++ src/myHooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: "http://localhost:2626",
+    baseURL: "https://voic-vista-server.vercel.app",
     withCredentials: true
 });
 
--- src/myHooks/useAxiosSecure.jsx
+++ src/myHooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from './useAuth'
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:2626',
+    baseURL: 'https://voic-vista-server.vercel.app',
     withCredentials: true
 });
 
